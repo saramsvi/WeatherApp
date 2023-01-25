bb7d6ccf4f798c87c6be567357e2cf02d0ec84fd
Sara <sarasadatmsvi@gmail.com>
2023-01-25 12:19:27 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by MSVI on 25.01.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
